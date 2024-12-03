466c2a7c12b7798c38976c4527c387f9eb7bb08d
Kalyan3081 <162336115+Kalyan3081@users.noreply.github.com>
2024-12-03 13:56:48 +0530

    Update tour.jsx

--- src/components/tour.jsx
+++ src/components/tour.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from 'react';
 
 const Tour = ({ id, name, info, image, price, removeTour }) => {
     const [showMore, setShowMore] = useState(false);
