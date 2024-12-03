71541798d0fc1c87c9f04147d2075ce50193da7e
Kalyan3081 <162336115+Kalyan3081@users.noreply.github.com>
2024-12-03 13:54:28 +0530

    Update tour.jsx

--- src/components/tour.jsx
+++ src/components/tour.jsx
@@ -1,21 +1,28 @@
-import React, { useState } from 'react';
+import { useState } from "react";
 
 const Tour = ({ id, name, info, image, price, removeTour }) => {
     const [showMore, setShowMore] = useState(false);
 
     return (
-        <article className="tour">
+        <article className="tour" id={`tour-item-${id}`}>
             <img src={image} alt={name} />
             <div className="tour-info">
                 <h4>{name}</h4>
                 <h5 className="tour-price">${price}</h5>
-               <p id='#tour-item-para-rec6d6T3q5EBIdCfD'>
+                <p id={`tour-item-para-${id}`}>
                     {showMore ? info : `${info.substring(0, 200)}...`}
-                    <button className="toggle-btn" onClick={() => setShowMore(!showMore)}>
+                    <button
+                        className="toggle-btn"
+                        onClick={() => setShowMore(!showMore)}
+                    >
                         {showMore ? 'See less' : 'Show more'}
                     </button>
                 </p>
-                <button className="delete-btn" id='#delete-btn-rec6d6T3q5EBIdCfD' onClick={() => removeTour(id)}>
+                <button
+                    id={`delete-btn-${id}`}
+                    className="delete-btn"
+                    onClick={() => removeTour(id)}
+                >
                     Not Interested
                 </button>
             </div>
