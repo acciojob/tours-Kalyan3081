d1dea44d3eae284555a11c181757c24ad0bf6dad
Kalyan3081 <162336115+Kalyan3081@users.noreply.github.com>
2024-12-03 13:48:38 +0530

    Update tour.jsx

--- src/components/tour.jsx
+++ src/components/tour.jsx
@@ -9,13 +9,13 @@ const Tour = ({ id, name, info, image, price, removeTour }) => {
             <div className="tour-info">
                 <h4>{name}</h4>
                 <h5 className="tour-price">${price}</h5>
-                <p>
+               <p id='#tour-item-para-rec6d6T3q5EBIdCfD'>
                     {showMore ? info : `${info.substring(0, 200)}...`}
                     <button className="toggle-btn" onClick={() => setShowMore(!showMore)}>
                         {showMore ? 'See less' : 'Show more'}
                     </button>
                 </p>
-                <button className="delete-btn" onClick={() => removeTour(id)}>
+                <button className="delete-btn" id='#delete-btn-rec6d6T3q5EBIdCfD' onClick={() => removeTour(id)}>
                     Not Interested
                 </button>
             </div>
